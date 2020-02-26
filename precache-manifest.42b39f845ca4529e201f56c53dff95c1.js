6932972a54a005fe76632a7e6a27dadb84cb2380
Liam Southall <lsouthall116@gmail.com>
2020-02-26 16:29:26 +0000

    Updates

---
+++ precache-manifest.42b39f845ca4529e201f56c53dff95c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e23b0e2685933d21c7599ea64b1014",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "5a1141313ba3011cfb67",
+    "url": "/calculator/static/css/main.7d5358f6.chunk.css"
+  },
+  {
+    "revision": "fb47d7f831adc60454ad",
+    "url": "/calculator/static/js/2.8a130742.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calculator/static/js/2.8a130742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a1141313ba3011cfb67",
+    "url": "/calculator/static/js/main.97dd5fda.chunk.js"
+  },
+  {
+    "revision": "28828353795638a97109",
+    "url": "/calculator/static/js/runtime-main.63ccf6e2.js"
+  }
+]);
